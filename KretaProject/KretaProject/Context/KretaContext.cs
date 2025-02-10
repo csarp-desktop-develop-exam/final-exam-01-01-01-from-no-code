f1e4c5022b1c08f322ac6738963e40908865f3a5
gycsaba <gycsaba@SZT9-24>
2025-02-10 18:31:45 +0100

    DbContext

---
+++ KretaProject/KretaProject/Context/KretaContext.cs
@@ -0,0 +1,33 @@
+﻿using KretaProject.Models;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+
+
+namespace KretaProject.Context
+{
+    internal class KretaContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<SchoolClass> SchoolClasses { get; set; }
+
+        public KretaContext()
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // az orders.db fájlt át kell másolni a bin/Debug mappa megfelelő mappájába 
+            string dbPath = Path.Combine(Environment.CurrentDirectory, "kreta.db");
+            using (var connection = new SqliteConnection($"Data Source={dbPath}"))
+                optionsBuilder.UseSqlite($"Data Source={dbPath}");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Tábla nevének explicit megadása
+            modelBuilder.Entity<Student>().ToTable("student");
+            modelBuilder.Entity<SchoolClass>().ToTable("schoolclass");
+        }
+    }
+}
