0dbf81579902ff84cd363dca0ecd9ce5b29373d4
gycsaba <gycsaba@SZT9-24>
2025-02-10 18:16:41 +0100

    A menüpontok működnek

--- KretaProject/KretaProject/Models/Student.cs
+++ KretaProject/KretaProject/Models/Student.cs
@@ -2,5 +2,10 @@
 {
     public class Student
     {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public bool IsWoman { get; set; }=false;
+        public DateTime BirthDay { get; set; } =DateTime.MinValue;
+        public int SchoolClassId { get; set; } = -1;
     }
 }
